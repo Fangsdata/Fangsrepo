ce147eb7290cfd785ea2f75e83790b9e084e02f2
stefanorn <stefanorn92@gmail.com>
2020-01-22 22:15:29 +0000

    created xunit-test from template

---
+++ ShipReports/Fangsdata.UnloadingShipReports.WebApi.test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Fangsdata.UnloadingShipReports.WebApi.test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
