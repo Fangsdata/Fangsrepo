c2e03bdf1b6ca4fc0d5899ecff36c279e4af0b26
stefanorn <stefanorn92@gmail.com>
2020-02-12 18:49:09 +0000

    Created unit test for webapi

--- Aflinn/OffloadWebApi/Repository/OffloadRepoTest.cs
+++ Aflinn/OffloadWebApi/Repository/OffloadRepoTest.cs
@@ -16,9 +16,11 @@ namespace OffloadWebApi.Repository
             {
                 return null;
             }
+
             var testFish = new List<FishDto>();
 
-            testFish.Add(new FishDto {
+            testFish.Add(new FishDto
+            {
                 Id = 1,
                 Application = "Mel og olje",
                 Condition = "Rund",
@@ -67,28 +69,29 @@ namespace OffloadWebApi.Repository
             {
                 Latitude = 55.5467,
                 Longitude = 3.28022,
-                Time = new DateTime(2019, 1, 1, 7, 45, 0)
+                Time = new DateTime(2019, 1, 1, 7, 45, 0),
             });
             testMapData.Add(new MapDataDto
             {
                 Latitude = 55.5430,
                 Longitude = 3.28500,
-                Time = new DateTime(2019, 1, 1, 7, 50, 0)
+                Time = new DateTime(2019, 1, 1, 7, 50, 0),
             });
             testMapData.Add(new MapDataDto
             {
                 Latitude = 55.5000,
                 Longitude = 3.28500,
-                Time = new DateTime(2019, 1, 1, 7, 55, 0)
+                Time = new DateTime(2019, 1, 1, 7, 55, 0),
             });
             testMapData.Add(new MapDataDto
             {
                 Latitude = 55.4900,
                 Longitude = 3.28600,
-                Time = new DateTime(2019, 1, 1, 8, 00, 0)
+                Time = new DateTime(2019, 1, 1, 8, 00, 0),
             });
 
-            var testBoat = new BoatDto {
+            var testBoat = new BoatDto
+            {
                 Id = 1,
                 Name = "Valdimar H",
                 RadioSignalId = "LK5707",
@@ -104,7 +107,8 @@ namespace OffloadWebApi.Repository
                 MapData = testMapData,
             };
 
-            var testData = new OffloadDetailDto {
+            var testData = new OffloadDetailDto
+            {
                 Id = id,
                 Town = "Vikna",
                 State = "Trøndelag",
