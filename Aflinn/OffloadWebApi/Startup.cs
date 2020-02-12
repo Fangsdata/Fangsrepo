715bfdb50a8a1002291c2d108afe2c4e3cef2d7a
stefanorn <stefanorn92@gmail.com>
2020-02-12 19:03:30 +0000

    injectaði dependencyum

--- Aflinn/OffloadWebApi/Startup.cs
+++ Aflinn/OffloadWebApi/Startup.cs
@@ -10,6 +10,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using OffloadWebApi.Controllers;
+using OffloadWebApi.Services;
+using OffloadWebApi.Repository;
 
 namespace OffloadWebApi
 {
@@ -26,6 +29,8 @@ namespace OffloadWebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<IOffloadRepo, OffloadRepoTest>();
+            services.AddScoped<IOffloadService, OffloadService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
