8fde52e1f3d67dbcfe47c0a09f51eff47dab7e2e
stefanorn <stefanorn92@gmail.com>
2020-02-18 08:52:15 +0000

    Styled the list

--- Aflinn/FangsdataWebSite/web-app/src/compoments/OffladsListItem/index.js
+++ Aflinn/FangsdataWebSite/web-app/src/compoments/OffladsListItem/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const OffloadListItem = ({ item }) => (
-        <div>
-            <img src={item.boatImage} alt= "a boat"/>
-            <p> {item.boatName}</p>
-            <p> {item.totalWeight}</p>
-            <p> {item.largestLanding}</p>
-            <p> {item.trips}</p>
+const OffloadListItem = ({ item, index }) => (
+        <div className="row">
+            <p className="col-0">{index}</p>
+            <img className="col-1 rounded" src={item.boatImage} alt= "a boat"/>
+            <p className="col"> {item.boatName}</p>
+            <p className="col-2"> {item.totalWeight} kg</p>
+            <p className="col-2"> {item.largestLanding} kg</p>
+            <p className="col-1"> {item.trips}</p>
         </div>
 );
 
--- Aflinn/FangsdataWebSite/web-app/src/compoments/OffloadsList/index.js
+++ Aflinn/FangsdataWebSite/web-app/src/compoments/OffloadsList/index.js
@@ -3,8 +3,16 @@ import OffladsListItem from '../OffladsListItem';
 
 const OffladsList = ({ offloads }) => (
     <div>
-        { offloads.map(item => <OffladsListItem 
-            key = {item.boatRadioSignalId} item = {item} />)}
+        <div className="row">
+        <p className="col-0"></p>
+            <p className="col-1"></p>
+            <p className="col">Name</p>
+            <p className="col-2">Total Offloaded</p>
+            <p className="col-2">Largest offloading</p>
+            <p className="col-1">Trips</p>
+        </div>
+        { offloads.map((item, index) => <OffladsListItem 
+            key = {item.boatRadioSignalId} item = {item} index={index} />)}
     </div>
 );
 
