05c6d55830753c7d9bba4602db162e47529ce6ae
antonbui <anton17@ru.is>
2020-02-28 14:27:51 +0000

    Navbar style changes

--- Aflinn/FangsdataWebSite/web-app/src/index.js
+++ Aflinn/FangsdataWebSite/web-app/src/index.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './Styles/base.css';
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 
